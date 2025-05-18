5531e8a7a75ed940cd32d85594b50e31b89452c7
Alex Leestemaker <alexleestemaker@gmail.com>
2025-05-19 00:07:46 +0200

    HandHolder item core implementation

---
+++ scripts/core/pieces/items/OnMove/HandHolder.cs
@@ -0,0 +1,44 @@
+﻿using CHESS2THESEQUELTOCHESS.scripts.core.utils;
+
+namespace CHESS2THESEQUELTOCHESS.scripts.core.pieces.items.OnMove;
+
+public class HandHolder(byte pieceId) : AbstractItem(pieceId, ItemTriggers.ON_MOVE)
+{
+    private int boardWidth, boardHeight;
+    private Piece piece;
+    
+    public override Board Execute(Board board, Move move)
+    {
+        // Find adjecent pieces with the same base piece type and color, try to move them with you
+        piece = board.GetPiece(PieceId);
+        Vector2Int moveDelta = move.To - move.From;
+
+        boardWidth = board.Squares.GetLength(0);
+        boardHeight = board.Squares.GetLength(1);
+
+        Vector2Int left = new(move.From.X - 1, move.From.Y);
+        AttemptPieceMove(board, left, moveDelta);
+        Vector2Int right = new(move.From.X + 1, move.From.Y);
+        AttemptPieceMove(board, right, moveDelta);
+        
+        return board;
+    }
+
+    private void AttemptPieceMove(Board board, Vector2Int position, Vector2Int delta)
+    {
+        if (!position.Inside(boardWidth, boardHeight))
+            return;
+        Piece toBeMoved = board.Squares[position.X, position.Y];
+        if (toBeMoved is null || toBeMoved.BasePiece != piece.BasePiece || toBeMoved.Color != piece.Color)
+            return;
+        Vector2Int goalPos = position + delta;
+        if (!goalPos.Inside(boardWidth, boardHeight))
+            return;
+        if (board.Squares[goalPos.X, goalPos.Y] is not null)
+            return;
+
+        toBeMoved.Position = goalPos;
+        board.Squares[position.X, position.Y] = null;
+        board.Squares[goalPos.X, goalPos.Y] = toBeMoved;
+    }
+}
